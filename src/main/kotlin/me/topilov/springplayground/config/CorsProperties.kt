96fbe704166b40639e1fad3c1db9784a65560aee
topilov <asynctopilov@gmail.com>
2026-03-20 22:55:50 +0600

    update cors allowed origins

--- src/main/kotlin/me/topilov/springplayground/config/CorsProperties.kt
+++ src/main/kotlin/me/topilov/springplayground/config/CorsProperties.kt
@@ -4,5 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("app.cors")
 data class CorsProperties(
-    val allowedOrigins: List<String> = listOf("http://localhost:4173"),
+    val allowedOrigins: List<String> = listOf(
+        "http://localhost:3000",
+        "http://localhost:4173",
+        "http://localhost:5173",
+    ),
 )
