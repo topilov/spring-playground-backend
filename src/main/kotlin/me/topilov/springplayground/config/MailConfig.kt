41de71e728428b45da13017a7cda3ac93be547ab
topilov <asynctopilov@gmail.com>
2026-03-23 01:52:30 +0600

    fix test profile

--- src/main/kotlin/me/topilov/springplayground/config/MailConfig.kt
+++ src/main/kotlin/me/topilov/springplayground/config/MailConfig.kt
@@ -4,8 +4,10 @@ import com.resend.Resend
 import me.topilov.springplayground.mail.MailProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("!test")
 class MailConfig {
 
     @Bean
